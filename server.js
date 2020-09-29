a480eb1391c0811c787eca44ffae5789e2f40616
angelask <angela.strayhorn@yahoo.com>
2020-09-29 17:30:57 -0500

    connected database

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost/dbFitness", { 
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/dbFitness", { 
     useNewUrlParser: true,
     useUnifiedTopology: true
 });
